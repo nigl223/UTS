35d213d5455c0311d4a346946f807b49984f6399
nigl223 <166437676+nigl223@users.noreply.github.com>
2025-01-07 22:42:56 +0700

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$dbname = 'pj_nigel';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
